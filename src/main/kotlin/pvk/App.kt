b635209b4c45dc46138d3ee85fd6d80cda82ca25
Mark Zhitomirski <mz@exactpro.com>
2020-06-30 23:25:28 +0300

    read PCAP file and print trivial stats

--- src/main/kotlin/pvk/App.kt
+++ src/main/kotlin/pvk/App.kt
@@ -1,30 +1,31 @@
 package pvk
 
 import java.io.File
-import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.lang.IllegalStateException
 import kotlin.system.exitProcess
+import org.apache.logging.log4j.LogManager
 
 class App {
-    val greeting: String
-        get() {
-            return "boo"
-        }
+    companion object {
+        val log = LogManager.getLogger()
+    }
+    val version = "0.1"
+}
 
-    fun ByteArray.getUIntAt(idx: Int): UInt =
-        ((this[idx  ].toUInt() and 0xFFu) shl 24) or
+fun ByteArray.getUIntAt(idx: Int): UInt =
+   if (isForeignByteOrder!!)
+        ((this[idx+3].toUInt() and 0xFFu) shl 24) or
+        ((this[idx+2].toUInt() and 0xFFu) shl 16) or
+        ((this[idx+1].toUInt() and 0xFFu) shl 8)  or
+         (this[idx  ].toUInt() and 0xFFu)
+   else ((this[idx  ].toUInt() and 0xFFu) shl 24) or
         ((this[idx+1].toUInt() and 0xFFu) shl 16) or
         ((this[idx+2].toUInt() and 0xFFu) shl 8)  or
          (this[idx+3].toUInt() and 0xFFu)
 
-    fun ByteArray.toHexString() = asUByteArray().joinToString("") { it.toString(16).padStart(2, '0') }
-
-    fun getMagic(f: FileInputStream): String {
-        var bb = ByteArray(16)
-        var br = f.read(bb,0,16)
-        var sig = ByteArray(4)
-        return "${bb.toHexString()}}"
-    }
-}
+fun ByteArray.toHexString() = asUByteArray().joinToString("") { it.toString(16).padStart(2, '0') }
 /*
 https://wiki.wireshark.org/Development/LibpcapFileFormat
 typedef struct pcap_hdr_s {
@@ -60,7 +61,52 @@ fun isPcapFile(magic: ByteArray): Boolean  {
         nanoTimestamped = true; isForeignByteOrder = true; return true
     } else return false
 }
+/* typedef struct pcaprec_hdr_s {
+ 0  guint32 ts_sec;         // timestamp seconds
+ 4  guint32 ts_usec;        // timestamp microseconds
+ 8  guint32 incl_len;       // number of octets of packet saved in file
+12  guint32 orig_len;       // actual length of packet
+} pcaprec_hdr_t;*/
+fun examine(s: InputStream): String {
+    if (isForeignByteOrder == null ) throw IllegalStateException("examine() cannot proceed: byte-order flag is 'null'")
+    val hlen = 16
+    var truncated = 0;
+    var pkts = 0
+    var hdrs = 0
+    var bhdr = ByteArray(16)
+    var pBuf = ByteArray(65536)
+    var hread = 0
+    var pread = 0
+    var maxLen = 0
+    var incl: Long = 0
+    try {
+        while (true) {
+            hread = s.read(bhdr)
+            if (hread == -1)  {
+                App.log.info("File have ended (-1 Bytes read)")
+                return "examined $pread packets. Longest $maxLen Bytes"
+            } else if (hread < hlen) {
+                return "Error reading header ${hdrs}. ${hread} Bytes read, "
+            }
+            hdrs++
+            println("Pcap_hdr${hdrs.toString().padStart(4, '.')}: ${bhdr.toHexString()}")
+            App.log.info("incl_len field: ${bhdr.slice(8..11).toByteArray().toHexString()}")
 
+            var incl = bhdr.getUIntAt(8)
+            App.log.info("Try reading $incl Bytes")
+            val pLen = s.read(pBuf, 0, incl.toInt())
+            if (incl.toInt() == pLen) {
+                pread++
+                maxLen = if (pLen > maxLen) pLen else maxLen
+            } else {
+                return "error reading packet ${hdrs}. $pLen Bytes read of ${incl}"
+            }
+        }
+    } catch (e: IOException) {
+        return "examined OK $pread packets. Error: ${e.message}"
+    }
+    return "unexpected return. examined $pread packets. Longest $maxLen Bytes"
+}
 
 fun main(args: Array<String>) {
     val pcapName = "/tmp/try10.pcap" // "/tmp/OUCH_tcpdump-20200501-000116.pcap"
@@ -69,15 +115,17 @@ fun main(args: Array<String>) {
     val f = File(pcapName)
     val fis = File(pcapName).inputStream()
     val flen = f.length()
-    var bb = ByteArray(2048)
+    var bb = ByteArray(24)
 
     if (flen < 24) {
         println("File has less than 24 bytes. Exiting"); exitProcess(1)
     }
-    var bytesRead = fis.read(bb)
+    var bytesRead = fis.read(bb,0,24)
     if (bytesRead > 23 && isPcapFile(bb.sliceArray(0..3))) {
         println("file ${pcapName} is a file: ${f.isFile}, has $flen bytes, read: $bytesRead Bytes.\n" +
-                "magic is OK. nanosecond timestamps: ${nanoTimestamped}; reverse: $isForeignByteOrder")
+                "magic is OK. nanosecond timestamps: ${nanoTimestamped}; reverse byte-order: $isForeignByteOrder")
+        App.log.info("Raw file header: ${bb.toHexString()}")
+        println(examine(fis))
     } else {
         println("PCAP magic not found")
     }
